1837d737a9fb139ae3b15a6abef88c3070ee0c01
BlackFeather <she3354248@yahoo.com.tw>
2016-12-11 21:17:01 +0800

    finish

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Api::UsersController < Api::BaseController
 	#$.get('http://localhost:3000/api/users.json', null, function(data) { console.log(data); });
 	# $.ajax({
@@ -24,39 +23,4 @@ class Api::UsersController < Api::BaseController
 		render json: { user: @user } #, status: 40
 	end
 end
-||||||| merged common ancestors
-=======
-class Api::UsersController < Api::BaseController
-	#$.get('http://localhost:3000/api/users.json', null, function(data) { console.log(data); });
-	# $.ajax({
-	#   url: "http://api.localhost:3000/api/users/3.json",
-	#   method: "POST",
-	#   success: function(data) { console.log(data) }
-	# })
-	# data: { turbolinks: 'false' }
-	before_action(except: [:index]) { add_crumb('Users', admin_users_path) }
 
-
-
-	def index
-		@users = User.all
-		render json: { users: @users }
-	end
-
-	def show 
-		#response.headers['Content-Type'] = "HTML"
-		@user = User.find(params[:id])
-		render json: { user: @user } #, status: 40
-	end
-
-		
-	def create
-	  	@user = User.new(params.require(:user).permit(:email , :password))
-	  	if @user.save
-	  		render json: {user: @user}
-	  	else
-	  		respond_error messages :@user.errors.full_messages
-	  	end
-  	end
-end
->>>>>>> feature
